3b289493c05af062eece9722390d0a8d30fc663e
moursalinme <me.moursalin@gmail.com>
2024-05-21 08:37:56 +0600

    completed swagger docs

--- app.js
+++ app.js
@@ -1,13 +1,41 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
-const AppError = require('./utils/AppError.js');
+const AppError = require('./utils/appError.js');
 const globalErrorController = require('./controllers/errorController.js');
+
+const swaggerjsdoc = require('swagger-jsdoc');
+const swaggerui = require('swagger-ui-express');
+// const { version } = require('mongoose');
+
+
+const swaggerOptions = {
+    swaggerDefinition : {
+        openapi: '3.0.0', 
+        info: {
+            version: '1.0.0', 
+            title: 'Swagger Setup', 
+            description: 'Swagger Documentation of Hotel management System API.', 
+            contact: {
+                name: 'Moursalin', 
+
+            }, 
+            servers: ['http://localhost:3030'],
+        },
+        schemes: ['http', 'https'],
+    },
+    apis: ['/routes/*.js',]
+};
+
+
 // Create App
 const app = express();
 
 // Enable JSON format
 app.use(express.json());
 app.use(cookieParser());
+
+const swaggerDocs = swaggerjsdoc(swaggerOptions);
+app.use('/api-docs', swaggerui.serve, swaggerui.setup(swaggerDocs));
 
 
 app.all('*', (req, res, next) => {
